b05c4bd26ced3e7df0e1846aa192322362d13210
guilherme soares <guilherme.dev36@gmail.com>
2022-11-15 15:49:37 -0300

    add typescript

---
+++ src/server.js
@@ -0,0 +1,18 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+//localhost
+app.get('/ads', (request, response) => {
+    return response.json([
+        { id: 1, name: 'anuncio 01' },
+        { id: 2, name: 'anuncio 02' },
+        { id: 3, name: 'anuncio 03' },
+        { id: 4, name: 'anuncio 04' },
+        { id: 5, name: 'anuncio 05' },
+    ]);
+});
+app.listen(3333);
